402aa5095b09a4d7db16e6b43d450b2682aa604e
Jabame <jabameyumiko@gmail.com>
2022-05-25 23:10:12 +0800

    features

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const ejs = require('ejs');
-const mysql = require('mysql');
+
 const app = express(); 
 
 
@@ -10,29 +10,15 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-const req = require('express/lib/request');
-
 
 let subjects = [];
+
 
 app.get('/', (req, res) => {
 
     res.render('home')
 
 });
-
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'test'
-  })
-  
-  connection.connect(function(err){
-      if(err) throw err;
-      console.log('Connected...');
-  })
 
 
 app.get('/profile', (req, res) => {
@@ -43,41 +29,38 @@ app.get('/profile', (req, res) => {
 });
 
 
-app.get('/add', (req, res) => {;
-    
+app.get('/add', (req, res) => {
+
     res.render('addSub')
 });
 
 
 app.get('/delete', (req, res) => {
-    
+
     res.render('deleteSub');
 });
+
+
 
 app.get('/change', (req, res) => {
 
     res.render('changeSub');
 })
 
-app.delete('/delete', (req, res) => {
-    res.send('Subject Deleted')
-  })
+
 
 app.post('/add', (req, res) => {
 
     const arrSubject = {
+
         title : req.body.title,
         code : req.body.code
     }
-    
-    var sql = "INSERT INTO subject VALUES('"+ req.body.title +"', '"+ req.body.code +"')"
 
-    connection.query(sql, (err, row, fields) => {
-        if (err) throw err
-        console.log();
-    })
     subjects.push(arrSubject);
+
     res.redirect('/profile')
+
 })
 
 
